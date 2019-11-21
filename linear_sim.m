14ee105ee3305550f2f93f2aa691d144ee47e67e
Michal <szpunar98@gmail.com>
2019-11-21 13:07:07 +0100

    correct linear simulation

--- linear_sim.m
+++ linear_sim.m
@@ -1,7 +1,25 @@
-function [outputArg1,outputArg2] = linear_sim(inputArg1,inputArg2)
-%LINEAR_SIM Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [y,t,x] = linear_sim(G,F1,FD,step_time, init_values, sim_time)
+
+t = 0:0.1:sim_time;
+signal_changes = zeros(length(t),2);
+signal_changes(1,:) = [F1(1), FD(1)];
+current_F1 = F1(1);
+current_FD = FD(1);
+
+for i=1:length(t)
+    if t(i) == step_time(1) && t(i) == step_time(2)
+        current_F1 = F1(2);
+        current_FD = FD(2);
+    elseif t(i) == step_time(1) 
+        current_F1 = F1(2);
+    elseif  t(i) == step_time(2)
+        current_FD = FD(2);
+    end
+    
+    signal_changes(i,:) = [current_F1, current_FD];
+end
+
+
+[y,t,x]=lsim(G,signal_changes,t,init_values);
 end
 
--- main.m
+++ main.m
@@ -8,19 +8,28 @@ h2p=7.98;
 h1p=11.7;
 Ts = 0.1;
 
- 
-F1=[F1p, F1p+10];
-FD=[FDp, FDp+10];
+F1_step_val = 10;
+FD_step_val = 20;
+
+
+F1=[F1p, F1p+ F1_step_val];
+FD=[FDp, FDp+ FD_step_val];
 step_time = [500, 1200];
 sim_time = 5000;
 [t1,x1] = nonlinear_sim(F1,FD,tau, step_time,[h1p, h2p], sim_time);
  
+
 figure(1);
+hold on;
 plot(t1,x1);
 legend('h1','h2');
 xlabel('Czas [s]');
 ylabel('Wysokoœæ [cm]');
  
+F1=[0, F1_step_val];
+FD=[0, FD_step_val];
 
 G_lin = linear_model([h1p,h2p], F1p, FDp, tau);
-step(G_lin)
+[y,t,x] = linear_sim(G_lin, F1, FD, step_time, [0, 0], sim_time);
+x = x + ones(size(x)).*[h1p,h2p];
+plot(t,x(:,2));
